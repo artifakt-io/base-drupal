f168f21969dcac9680f0f6da489814e8c2a79e86
djalal <djalal@users.noreply.github.com>
2021-05-20 16:02:06 +0200

    upgrade to 8.9.15 (#10)
    * init container version
    
    * chmod bash scripts
    
    * Update entrypoint.sh
    
    * install needs a native setting file
    
    * Update build.sh
    
    * Update build.sh
    
    * bash is not php :p
    
    * fix: default folder does not exist (yet)
    
    * debug missing file
    
    * force default settings at entrypoint
    
    * upgrade to 8.9.15

---
+++ .artifakt/build.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -e
+
+echo ">>>>>>>>>>>>>> START CUSTOM BUILD SCRIPT <<<<<<<<<<<<<<<<< "
+
+echo "------------------------------------------------------------"
+echo "The following build args are available:"
+env
+echo "------------------------------------------------------------"
+
+# NO SCRIPTS, it breaks the build
+# see https://stackoverflow.com/a/61349991/1093649
+composer --no-ansi --no-interaction install --no-cache --no-progress  --no-autoloader --no-scripts --no-dev
+
+# install drush CLI as an additional package
+composer require --no-cache --update-no-dev --prefer-dist --no-ansi --no-interaction drush/drush && \
+composer dump-autoload
+
+apt-get update && apt-get install -y --no-install-recommends gnupg
+
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - \
+    && echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list \
+    && apt-get update && apt-get install --no-install-recommends -y nodejs yarn \
+    && rm -rf /var/lib/apt/lists/*
+
+if [[ ! -f /data/sites/default/default.settings.php ]]; then
+  mkdir -p /data/sites/default
+  cp /.artifakt/default.settings.php /data/sites/default/default.settings.php
+  ls -la /data/sites/default
+fi
+
+if [[ ! -d /opt/drupal/config ]]; then
+  ln -s /data/config /opt/drupal/config
+fi
+
+chown -R www-data:www-data /var/www/html/
+
+echo ">>>>>>>>>>>>>> END CUSTOM BUILD SCRIPT <<<<<<<<<<<<<<<<< "
