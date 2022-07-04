b600d17f7a7f4729f688bf9037dc84e53bc90f70
djalal <djalal@users.noreply.github.com>
2022-07-04 16:35:59 +0200

    add debug

--- .artifakt/entrypoint.sh
+++ .artifakt/entrypoint.sh
@@ -5,4 +5,10 @@ echo ">>>>>>>>>>>>>> START CUSTOM ENTRYPOINT SCRIPT <<<<<<<<<<<<<<<<< "
 
 composer show
 
+ls -la sites/
+ls -la sites/default/
+ls -la sites/default/files
+
+
+
 echo ">>>>>>>>>>>>>> END CUSTOM ENTRYPOINT SCRIPT <<<<<<<<<<<<<<<<< "
