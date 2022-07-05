e9ab1d30e50316ae8d472576e3089c47ac2a559e
djalal <djalal@users.noreply.github.com>
2022-07-05 16:34:06 +0200

    Update entrypoint.sh

--- .artifakt/entrypoint.sh
+++ .artifakt/entrypoint.sh
@@ -5,10 +5,18 @@ echo ">>>>>>>>>>>>>> START CUSTOM ENTRYPOINT SCRIPT <<<<<<<<<<<<<<<<< "
 
 composer show
 
+echo "current dir:"
+pwd
+
+cd /opt/drupal
+
+mkdir -p sites/default/files
+
 ls -la sites/
 ls -la sites/default/
 ls -la sites/default/files
 
+chown -R www-data:www-data /opt/drupal/sites
 
 
 echo ">>>>>>>>>>>>>> END CUSTOM ENTRYPOINT SCRIPT <<<<<<<<<<<<<<<<< "
