4af73f633a24dc9bc3fcd8dd7139f19747b5c253
djalal <djalal@users.noreply.github.com>
2022-07-05 22:57:50 +0200

    Update entrypoint.sh

--- .artifakt/entrypoint.sh
+++ .artifakt/entrypoint.sh
@@ -16,6 +16,8 @@ ls -la sites/
 ls -la sites/default/
 ls -la sites/default/files
 
+su --preserve-environment www-data -s /bin/bash -c 'env && /opt/drupal/vendor/bin/drush site:install standard si --no-interaction --account-pass=password123 --db-url=mysql://$ARTIFAKT_MYSQL_USER:$ARTIFAKT_MYSQL_USERNAME@$ARTIFAKT_MYSQL_HOST:$ARTIFAKT_MYSQL_PORT/$ARTIFAKT_MYSQL_DATABASE_NAME'
+
 chown -R www-data:www-data /opt/drupal/sites
 
 
