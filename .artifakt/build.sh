e3ac48497af2de2e2cc70317d50099b1c1442218
djalal <djalal@users.noreply.github.com>
2022-07-05 22:56:04 +0200

    Update build.sh

--- .artifakt/build.sh
+++ .artifakt/build.sh
@@ -6,7 +6,11 @@ echo ">>>>>>>>>>>>>> START CUSTOM BUILD SCRIPT <<<<<<<<<<<<<<<<< "
 
 echo 'variables_order = "EGPCS"' >> /usr/local/etc/php/conf.d/zzz-artifakt.ini
 
+composer require drush/drush
+
 composer show
+
+chown -R www-data:www-data /opt/drupal/vendor
 
 if [[ ! -d /opt/drupal/config ]]; then
  mkdir -p /data/config
